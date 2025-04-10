ec767d4f94aa6de36f790ae7acdbcfa2d72fabf4
node-4 <node4@flyweis.technology>
2025-04-10 17:34:10 +0530

    update 10 april 2025

--- server.js
+++ server.js
@@ -99,6 +99,7 @@ mongoose.set("strictQuery", true);
 mongoose.connect(process.env.DB_URL).then((data) => {
     console.log(`Mongodb connected with server: ${data.connection.host}`);
 });
+
 app.listen(process.env.PORT, () => {
     console.log(`Listening on port ${process.env.PORT}!`);
 });
