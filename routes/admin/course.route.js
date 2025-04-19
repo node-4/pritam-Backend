b299b02d23de2ff82678b1209e47644dff8d1485
node-4 <node4@flyweis.technology>
2025-04-19 12:32:55 +0530

    update 17 april 2025

--- routes/admin/course.route.js
+++ routes/admin/course.route.js
@@ -20,15 +20,15 @@ const storage = new CloudinaryStorage({
 });
 const upload = multer({ storage: storage });
 module.exports = (app) => {
-        app.post('api/v1/course/addCourse', upload.array('image'), auth.AddCourse);
-        app.get('api/v1/course/all', auth.getCourses);
-        app.get('api/v1/course/view/:id', auth.getCoursesById);
-        app.put('api/v1/course/edit/:id/:tillDate', upload.array('image'), auth.editCourse);
-        app.get('api/v1/course/viewByTrendingServiceId/:trendingServiceId', auth.getCoursesbyTrendingServiceId);
-        app.delete('api/v1/course/delete/:id', auth.DeleteCourses);
-        app.post('api/v1/course/Skill/addSkill', auth.AddSkill);
-        app.get('api/v1/course/Skill/all', auth.getSkills);
-        app.get('api/v1/course/Skill/view/:id', auth.getSkillsById);
-        app.put('api/v1/course/Skill/edit/:id', auth.editSkill);
-        app.delete('api/v1/course/Skill/delete/:id', auth.DeleteSkills);
+        app.post('/api/v1/course/addCourse', upload.array('image'), auth.AddCourse);
+        app.get('/api/v1/course/all', auth.getCourses);
+        app.get('/api/v1/course/view/:id', auth.getCoursesById);
+        app.put('/api/v1/course/edit/:id/:tillDate', upload.array('image'), auth.editCourse);
+        app.get('/api/v1/course/viewByTrendingServiceId/:trendingServiceId', auth.getCoursesbyTrendingServiceId);
+        app.delete('/api/v1/course/delete/:id', auth.DeleteCourses);
+        app.post('/api/v1/course/Skill/addSkill', auth.AddSkill);
+        app.get('/api/v1/course/Skill/all', auth.getSkills);
+        app.get('/api/v1/course/Skill/view/:id', auth.getSkillsById);
+        app.put('/api/v1/course/Skill/edit/:id', auth.editSkill);
+        app.delete('/api/v1/course/Skill/delete/:id', auth.DeleteSkills);
 }
